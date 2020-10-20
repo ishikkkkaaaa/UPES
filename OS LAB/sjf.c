5b93c4ac242ae1d0dd7c7f307cce7f6e003ad595
ishika <imusun24@gmail.com>
2020-10-21 01:24:04 +0530

    21/10/20

--- OS LAB/sjf.c
+++ OS LAB/sjf.c
@@ -1,45 +1,53 @@
 #include<stdio.h>
-# define max 30
-int main()
+#define max 30
+int main(void)
 {
-  int i,j,n,temp1,temp2,p[max],bt[max],wt=0;
-  printf("Enter the number of prcoess:");
+  int i,j,n,t,p[max],bt[max],wt[max],tat[max];
+
+  printf("Enter the number of process:");
   scanf("%d",&n);
-  printf("process number \n");
-  for(i=0;i<n;i++)
+
+printf("Enter the Process number:");
+for(i=0;i<n;i++)
+{
+  scanf("%d",&p[i]);
+}
+printf("Enter the burst time of the process:");
+for(i=0;i<n;i++)
+{
+  scanf("%d",&bt[i]);
+}
+//applies bubble sort technique, to sort acc to their burst time
+for(i=0;i<n;i++)
+{
+  for(j=0;j<n-i-1;j++)
   {
-    scanf("%d",&p[i]);
-  }
-  printf("Enter the burst time of the process:\n");
-  for(i=0;i<n;i++)
-  {
-    scanf("%d",&bt[i]);
-  }
-  for(i=1;i<n;i++)
-  {
-    for(j=0;j<n-i;j++)
+    if(bt[j]>bt[j+1])
     {
-      if(bt[j]>bt[j+1])
-      {
-        temp1=bt[j];
-        bt[j]=bt[j+1];
-        bt[j+1]=temp1;
-        temp2=p[j];
-        p[j]=p[j+1];
-        p[j+1]=temp2;
-      }
+      //for burst time
+      t=bt[j];
+      bt[j]=bt[j+1];
+      bt[j+1]=t;
+
+      //for process number
+      t=p[j];
+      p[j]=p[j+1];
+      p[j+1]=t;
     }
   }
-	for(int j=0;j<n;j++){
-		if(j==0){
-			printf("The waiting time of process %d is: %d",p[j],wt);
-			printf("\n");
-	    }
-		else{
-			wt=wt+bt[j-1];
-			printf("The waiting time of process %d is: %d",p[j],wt);
-			printf("\n");
-			}
-	}
-  return(0);
+}
+printf("process\tburst time\twaiting time\tturn around time\n");
+for(i=0;i<n;i++)
+{
+  wt[i]=0;
+  tat[i]=0;
+  for(j=0;j<i;j++)
+  {
+    wt[i]=wt[i]+bt[j];
+  }
+tat[i]=wt[i]+bt[i];
+
+printf("%d\t %d\t\t %d\t\t %d\n",p[i],bt[i],wt[i],tat[i]);
+}
+return 0;
 }
